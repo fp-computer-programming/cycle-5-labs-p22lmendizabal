504c8d166340a658e5c8a408373bc02efd54cb30
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-27 10:20:43 -0400

    LAb 5

---
+++ lab_5-1.py
@@ -0,0 +1,8 @@
+# author: LM (AMDG) 10/27/21
+school = "Fairfield Prep"
+first_half = school[0, 9]
+second_half = school[9:]
+print(first_half)
+print(second_half)
+
+print(first_half + second_half)
---
+++ lab_5-2.py
@@ -0,0 +1,9 @@
+# author: LM (AMDG) 10/27/21
+fruit1 = print(input(" Enter a word. "))
+fruit2 = print(input("Enter another word. "))
+if fruit1 > fruit2:
+    print(fruit1 + " comes after " + fruit2)
+elif fruit1 <  fruit2:
+    print(fruit1 + " comes before " + fruit2)
+else:
+    print(fruit1 + " and " + fruit2 + " are the same.")
